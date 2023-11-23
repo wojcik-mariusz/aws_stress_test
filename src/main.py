6cda52211be9e609337972756dcb25cf199ad450
Mariusz <wojcikk.mariusz@gmail.com>
2023-11-23 18:32:02 +0100

    Include user_router

--- src/main.py
+++ src/main.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI
 
+from src.api.users import router as users_router
 
 app: FastAPI = FastAPI()
 
@@ -7,3 +8,6 @@ app: FastAPI = FastAPI()
 @app.get("/", status_code=200)
 def read_root() -> dict[str: str]:
     return {"message": "server running"}
+
+
+app.include_router(router=users_router.router)
