09b2c86a861b798136985dc8c06862835ad6833d
Mariusz Wojcik <wojcik.mariusz@int.pl>
2023-11-26 22:24:34 +0100

    Add prefix to router. Change endpoint address, name and message.

--- src/api/users/router.py
+++ src/api/users/router.py
@@ -1,8 +1,8 @@
 from fastapi import APIRouter
 
-router: APIRouter = APIRouter()
+router: APIRouter = APIRouter(prefix="/users")
 
 
-@router.get("/r", status_code=200)
-def home():
-    return {"message": "user_router ok"}
+@router.get("/all", status_code=200)
+def get_users_list():
+    return {"message": "get_all_users"}
