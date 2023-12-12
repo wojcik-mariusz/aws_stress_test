12ca88340036b24567722c53ce040a5f2a44f737
Mariusz W <wojcikk.mariusz@gmail.com>
2023-12-12 23:16:37 +0100

    Implement dependency injection by oauth2 bearer. Also add simple endpoint to check user credentials.

--- src/api/users/router.py
+++ src/api/users/router.py
@@ -1,8 +1,17 @@
-from fastapi import APIRouter
+from typing import Annotated
+from fastapi import APIRouter, Depends
+
+from src.api.users.security import oauth2_scheme, get_current_user
+from src.api.users.security import User
 
 router: APIRouter = APIRouter(prefix="/users")
 
 
 @router.get("/all", status_code=200)
-def get_users_list():
-    return {"message": "get_all_users"}
+def get_users_list(token: Annotated[str, Depends(oauth2_scheme)]):
+    return {"message": "get_all_users", "token": token}
+
+
+@router.get("/me", status_code=200)
+def read_user_me(current_user: Annotated[User, Depends(get_current_user)]):
+    return current_user
