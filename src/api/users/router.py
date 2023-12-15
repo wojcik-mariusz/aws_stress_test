503c82041e4b4c7afbb252e89c14c9d7b86e2119
Mariusz W <wojcikk.mariusz@gmail.com>
2023-12-16 00:25:18 +0100

    Add login form

--- src/api/users/router.py
+++ src/api/users/router.py
@@ -1,8 +1,11 @@
 from typing import Annotated
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, HTTPException, status
+from fastapi.security import OAuth2PasswordRequestForm
 
-from src.api.users.security import oauth2_scheme, get_current_user
+from src.api.users.security import oauth2_scheme, get_current_user, fake_hash_password
 from src.api.users.security import User
+from src.api.users.user_db import fake_users_db
+from src.api.users.user_schema import UserInDB
 
 router: APIRouter = APIRouter(prefix="/users")
 
@@ -15,3 +18,21 @@ def get_users_list(token: Annotated[str, Depends(oauth2_scheme)]):
 @router.get("/me", status_code=200)
 def read_user_me(current_user: Annotated[User, Depends(get_current_user)]):
     return current_user
+
+
+@router.post("/token")
+def login(form_data: Annotated[OAuth2PasswordRequestForm, Depends()]):
+    user_dict = fake_users_db.get(form_data.username)
+    if not user_dict:
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail="Incorrect username or password",
+        )
+    user = UserInDB(**user_dict)
+    hashed_password = fake_hash_password(form_data.password)
+    if not hashed_password == user.hashed_password:
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail="Incorrect username or password",
+        )
+    return {"access_token": user.username, "token_type": "bearer"}
