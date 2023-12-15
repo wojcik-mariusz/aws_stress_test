88fd2fc7ddee6089592b3ca29a6c5be3043c6da3
Mariusz W <wojcikk.mariusz@gmail.com>
2023-12-16 00:16:19 +0100

    Add functionality to get current active user.

--- src/api/users/security.py
+++ src/api/users/security.py
@@ -1,5 +1,5 @@
 from typing import Annotated
-from fastapi import Depends
+from fastapi import Depends, HTTPException, status
 from fastapi.security import OAuth2PasswordBearer
 from src.api.users.user_db import fake_users_db
 
@@ -8,21 +8,31 @@ from src.api.users.user_schema import User, UserInDB
 oauth2_scheme: OAuth2PasswordBearer = OAuth2PasswordBearer(tokenUrl="token")
 
 
-def get_user(db, username:str):
-        if username in db:
-            user_dict = db[username]
-            return UserInDB(**user_dict)
+def get_user(db, username: str):
+    if username in db:
+        user_dict = db[username]
+        return UserInDB(**user_dict)
 
 
 def fake_decode_token(token: str) -> User:
-    pass
-
-
-
-def get_current_user(token: Annotated[str, Depends(oauth2_scheme)]) -> User:
-    user = fake_decode_token(token)
+    user = get_user(fake_users_db, token)
     return user
 
 
-def fake_hash_password(password: str) -> str:
-    return "fakehashed" + password
+def get_current_user(token: Annotated[str, Depends(oauth2_scheme)]):
+    user = fake_decode_token(token)
+    if not user:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Invalid authentication credentials",
+            headers={"WWW-Authenticate": "Bearer"},
+        )
+    return user
+
+
+def get_current_active_user(current_user: Annotated[User, Depends(get_current_user)]):
+    if current_user.disabled:
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST, detail="Inactive user"
+        )
+    return current_user
