ba4023579622f7a87c5249bb84e36291f31e6f17
Mariusz W <wojcikk.mariusz@gmail.com>
2023-12-16 00:24:01 +0100

    Add functionality to fake hash password

--- src/api/users/security.py
+++ src/api/users/security.py
@@ -8,6 +8,10 @@ from src.api.users.user_schema import User, UserInDB
 oauth2_scheme: OAuth2PasswordBearer = OAuth2PasswordBearer(tokenUrl="token")
 
 
+def fake_hash_password(password: str):
+    return "fakehashed" + password
+
+
 def get_user(db, username: str):
     if username in db:
         user_dict = db[username]
