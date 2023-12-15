6b1b455c2e4d8a71e3bc02654fe7047dbe703c11
Mariusz Wojcik <wojcik.mariusz@int.pl>
2023-12-15 22:25:18 +0100

    Create get_user to hash user password. Also add functionality to hash password.

--- src/api/users/security.py
+++ src/api/users/security.py
@@ -1,22 +1,28 @@
 from typing import Annotated
 from fastapi import Depends
 from fastapi.security import OAuth2PasswordBearer
+from src.api.users.user_db import fake_users_db
 
-from src.api.users.user_schema import User
+from src.api.users.user_schema import User, UserInDB
 
 oauth2_scheme: OAuth2PasswordBearer = OAuth2PasswordBearer(tokenUrl="token")
 
 
+def get_user(db, username:str):
+        if username in db:
+            user_dict = db[username]
+            return UserInDB(**user_dict)
+
+
 def fake_decode_token(token: str) -> User:
-    return User(
-        username=token + "fakedecoded", email="john@appleseed.com", full_name="John Doe"
-    )
+    pass
 
 
-def get_current_user(token: Annotated[str, Depends(oauth2_scheme)]):
+
+def get_current_user(token: Annotated[str, Depends(oauth2_scheme)]) -> User:
     user = fake_decode_token(token)
     return user
 
-def fake_hash_password():
-    pass
 
+def fake_hash_password(password: str) -> str:
+    return "fakehashed" + password
