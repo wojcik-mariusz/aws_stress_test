e10b1e838fa2fb471303b3f9ec481fa318885718
Mariusz Wojcik <wojcik.mariusz@int.pl>
2023-12-17 13:40:56 +0100

    Functionality to equalize plain and hashed password, and get password hash.

--- src/api/users/security.py
+++ src/api/users/security.py
@@ -32,6 +32,12 @@ pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 oauth2_scheme: OAuth2PasswordBearer = OAuth2PasswordBearer(tokenUrl="users/token")
 
 
+def verify_password(plain_password, hashed_password):
+    return pwd_context.verify(plain_password, hashed_password)
+
+
+def get_password_hash(password: str):
+    return pwd_context.hash(password)
 
 
 def get_user(db, username: str):
