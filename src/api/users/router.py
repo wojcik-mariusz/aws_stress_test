cdf8dad8885378bace23820f4cb854ae91a38a4a
Mariusz Wojcik <wojcik.mariusz@int.pl>
2023-12-17 13:38:18 +0100

    Change functionality of token endpoint to use jwt.

--- src/api/users/router.py
+++ src/api/users/router.py
@@ -4,7 +4,13 @@ from typing import Annotated
 from fastapi import APIRouter, Depends, HTTPException, status
 from fastapi.security import OAuth2PasswordRequestForm
 
-from src.api.users.security import oauth2_scheme, get_current_user, fake_hash_password, get_current_active_user
+from src.api.users.security import (
+    oauth2_scheme,
+    get_current_user,
+    get_current_active_user,
+    authenticate_user,
+    create_access_token,
+)
 from src.api.users.security import User
 from src.api.users.user_db import fake_users_db
 
@@ -24,21 +30,18 @@ def read_user_me(current_user: Annotated[User, Depends(get_current_user)]):
 
 
 @router.post("/token")
-def login(form_data: Annotated[OAuth2PasswordRequestForm, Depends()]):
-    user_dict = fake_users_db.get(form_data.username)
-    if not user_dict:
+def login_for_access_token(form_data: Annotated[OAuth2PasswordRequestForm, Depends()]):
+    user = authenticate_user(fake_users_db, form_data.username, form_data.password)
+    if not user:
         raise HTTPException(
             status_code=status.HTTP_400_BAD_REQUEST,
             detail="Incorrect username or password",
         )
-    user = UserInDB(**user_dict)
-    hashed_password = fake_hash_password(form_data.password)
-    if not hashed_password == user.hashed_password:
-        raise HTTPException(
-            status_code=status.HTTP_400_BAD_REQUEST,
-            detail="Incorrect username or password",
-        )
-    return {"access_token": user.username, "token_type": "bearer"}
+    access_token_expires = datetime.timedelta(minutes=int(ACCESS_TOKEN_EXPIRE_MINUTES))
+    access_token = create_access_token(
+        data={"sub": user.username}, expires_delta=access_token_expires
+    )
+    return {"access_token": access_token, "token_type": "bearer"}
 
 
 @router.get("users/me", status_code=200)
