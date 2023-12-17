554a4010b1fc19b678464760c77ff8e4d8a51440
Mariusz Wojcik <wojcik.mariusz@int.pl>
2023-12-17 13:42:09 +0100

    Add function to create user access token.

--- src/api/users/security.py
+++ src/api/users/security.py
@@ -55,6 +55,17 @@ def authenticate_user(db, username: str, password: str):
     return user
 
 
+def create_access_token(data: dict, expires_delta: timedelta | None = None):
+    to_encode = data.copy()
+    if expires_delta:
+        expire = datetime.utcnow() + expires_delta
+    else:
+        expire = datetime.utcnow() + timedelta(minutes=15)
+    to_encode.update({"exp": expire})
+    encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
+    return encoded_jwt
+
+
 def get_current_user(token: Annotated[str, Depends(oauth2_scheme)]):
     user = fake_decode_token(token)
     if not user:
