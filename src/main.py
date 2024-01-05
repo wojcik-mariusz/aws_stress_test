28dec7f5e80b01a2b6c6bdd6224662220d0177c2
Mariusz Wojcik <wojcik.mariusz@int.pl>
2024-01-05 19:21:37 +0100

    Add Magnum handler.

--- src/main.py
+++ src/main.py
@@ -1,8 +1,10 @@
 from fastapi import FastAPI
+from mangum import Mangum
 
 from src.api.users.router import router as user_router
 
 app: FastAPI = FastAPI()
+handler = Mangum(app)
 
 
 @app.get("/", status_code=200)
